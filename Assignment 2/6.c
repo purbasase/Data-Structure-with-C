b0e795699b511bbe4ec0a013c85e8938c0ad79ac
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-08 14:17:57 +0530

    Create 6.c

---
+++ Assignment 2/6.c
@@ -0,0 +1,30 @@
+//c program to display n numbers of elements .Memory should be allocated dynamically using malloc()
+
+ #include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The elements are:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
